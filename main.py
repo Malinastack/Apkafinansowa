cc4589a7af57e462fc599a3771f31529598e8c34
Malinastack <mszymon962@gmail.com>
2025-02-13 21:39:41 +0100

    Added some relationships

--- main.py
+++ main.py
@@ -1,46 +1,116 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Query
 from database import engine
 from sqlmodel import Session, SQLModel, select
-from models import User, Expense, UserCreate, UserPublic
+from models import User, Expense, UserCreate, UserPublic, UserUpdate, ExpenseCreate, ExpensePublic, ExpenseUpdate
 
 app = FastAPI()
 
+def get_session():
+    with Session(engine) as session:
+        yield session
+
 SQLModel.metadata.create_all(engine)
+
+def hash_password(password: str) -> str:
+    # Use something like passlib here
+    return f"not really hashed {password} hehehe"
 
 @app.get("/")
 def root():
     return "Siemanko"
 
 @app.post("/create_user", response_model=UserPublic)
-async def create_user(user: UserCreate):
-    with Session(engine) as session:
-        db_user = User.model_validate(user)
-        session.add(db_user)
-        session.commit()
-        session.refresh(db_user)
-        return db_user
+async def create_user(*, session: Session = Depends(get_session), user: UserCreate):
+    hashed_password = hash_password(user.password)
+    extra_data = {"hashed_password": hashed_password}
+    db_user = User.model_validate(user, update=extra_data)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
 
-@app.post("/create_expense")
-async def create_expense(expense: Expense):
-    with Session(engine) as session:
-        session.add(expense)
-        session.commit()
-        session.refresh(expense)
-        return expense
-    
-
-@app.get("/get_users", response_model=list[UserPublic])
-async def get_users():
-    with Session(engine) as session:
-        results = session.exec(select(User)).all()
-        return results
+@app.get("/users", response_model=list[UserPublic])
+async def get_users(*, session: Session = Depends(get_session)):
+    results = session.exec(select(User)).all()
+    return results
     
 @app.get("/users/{user_id}", response_model=UserPublic)
-async def get_user(user_id:int):
-    with Session(engine) as session:
-        user = session.get(User, user_id)
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found")
-        return user
+async def get_user(*, session: Session = Depends(get_session), user_id:int):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+    
+@app.patch("/users/{user_id}", response_model=UserPublic)
+async def update_user(*, session: Session = Depends(get_session), user_id: int, user: UserUpdate):
+    db_user = session.get(User, user_id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    user_data = user.model_dump(exclude_unset=True)
+    extra_data = {}
+    if "password" in user_data:
+        password = user_data["password"]
+        hashed_password = hash_password(password)
+        extra_data["hashed_password"] = hashed_password
+    db_user.sqlmodel_update(user_data, update=extra_data)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
+
+@app.delete("/users/{user.id}")
+async def delete_user(*, session: Session = Depends(get_session), user_id: int):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    session.delete(user)
+    session.commit()
+    return {"User deleted": True}
+
+@app.post("/create_expense", response_model=ExpensePublic)
+async def create_expense(*, session: Session = Depends(get_session), expense: ExpenseCreate):
+    db_expense = Expense.model_validate(expense)
+    session.add(db_expense)
+    session.commit()
+    session.refresh(db_expense)
+    return db_expense
+
+@app.get("/expenses", response_model= list[ExpensePublic])
+async def get_expenses(*, session: Session = Depends(get_session)):
+    result = session.exec(select(Expense)).all()
+    return result
+
+@app.get("/expenses/{expense_id}", response_model=ExpensePublic)
+async def get_expense(*, session: Session = Depends(get_session), expense_id: int):
+    db_expense = session.get(Expense, expense_id)
+    if not db_expense:
+        return HTTPException(status_code=404, detail="Expense not found")
+    return db_expense
+    
+@app.patch("/expense/{expense_id}", response_model=ExpensePublic)
+async def update_expense(*, session: Session = Depends(get_session), expense_id: int, expense: ExpenseUpdate):
+    db_expense = session.get(Expense, expense_id)
+    if not db_expense:
+        return HTTPException(status_code=404, detail="Expense not found")
+    expense_data = expense.model_dump(exclude_unset=True)
+    for key, value in expense_data.items():
+        setattr(db_expense, key, value)
+    session.add(db_expense)
+    session.commit()
+    session.refresh(db_expense)
+    return db_expense
+
+@app.delete("/expense/{expense_id}")
+async def delete_expense(*, session: Session = Depends(get_session), expense_id: int):
+    db_expense = session.get(Expense, expense_id)
+    if not db_expense:
+        return HTTPException(status_code=404, detail="Expense not found")
+    session.delete(db_expense)
+    session.commit()
+    return {"Expense deleted": True}
+
+
+
+
         
 
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
 
 class UserBase(SQLModel):
     name: str
@@ -6,16 +6,38 @@ class UserBase(SQLModel):
 
 class User(UserBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    hashed_password: str = Field()
+    expense: list["Expense"] = Relationship(back_populates="user")
 
 class UserCreate(UserBase):
-    pass
+    password: str
 
 class UserPublic(UserBase):
     id: int
 
-class Expense(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+class UserUpdate(SQLModel):
+    name: str | None = None
+    balance: float | None = None
+    password: str | None = None
+    
+class ExpenseBase(SQLModel):
     name: str
     amount: float = Field(default=0.0)
-    payer_id: int
+    user_id: int | None = Field(default=None, foreign_key="user.id")
+
+class Expense(ExpenseBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    user: User | None = Relationship(back_populates="expense")
+
+class ExpenseCreate(ExpenseBase):
+    pass
+
+class ExpensePublic(ExpenseBase):
+    id: int
+
+class ExpenseUpdate(SQLModel):
+    name: str | None = None
+    amount: float | None = None
+    user_id: int | None = None
+
 
